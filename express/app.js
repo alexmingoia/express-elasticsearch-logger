51e885f377222f58a8267b96b0b9fe073da5d973
Putt <junrai82@gmail.com>
2018-07-20 14:48:01 +0700

    update mapping syntax

--- express/app.js
+++ express/app.js
@@ -2,12 +2,69 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+var expressLogger = require("../lib/express-elasticsearch-logger")
 
 var app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+var loggerHost = "http://localhost:9200"
+app.use(expressLogger.requestHandler({
+  host: loggerHost,
+  whitelist:{
+    request: ['body', 'email', 'httpVersion', 'headers', 'method', 'originalUrl', 'path', 'query'],
+    response: ['statusCode', 'sent']
+  },
+  censor: ['ssn', "socialSecurityNumber", 'password', 'form.socialSecurityNumber', 'userInfo.password'],
+  mapping: {
+    properties: {
+      response: {
+        properties: {
+          sent: {
+            type: 'object',
+            enabled: false
+          }
+        }
+      },
+      request: {
+        properties: {
+          email: {
+            type: 'text'
+          }
+        }
+      },
+      error: {
+        properties: {
+          errors: {
+            type: 'object',
+            enabled: false
+          }
+        }
+      }
+    }
+  }
+}))
+
+app.get("/", function(req, res) {
+  res.send(new Date())
+})
+
+var router = express.Router()
+
+router.get("/", function(req, res) {
+  res.send(req.query)
+})
+
+router.post("/", function(req, res) {
+  res.send(req.body)
+})
+
+app.use("/route", router)
+
+
+app.use(expressLogger.errorHandler);
 
 
 // catch 404 and forward to error handler
