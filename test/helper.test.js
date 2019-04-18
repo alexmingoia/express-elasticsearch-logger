42afba75c9426e7614c554a69aa0d47897ad6be3
Jirayu Roungsuriyaviboon <odlho4869@gmail.com>
2019-04-18 15:45:48 +0700

    handle array with position * for censor function

--- test/helper.test.js
+++ test/helper.test.js
@@ -35,6 +35,138 @@ describe('helper module', function () {
         var newObj = helper._censorDeep(original, 'a.azd.ewx.1.2'.split('.'));
         expect(newObj).to.have.deep.equal(original);
       });
+
+      it('should be able to censor data in array with * position and nested object', function() {
+        var original = {
+          Questions: [
+            {
+              question: "this is q1",
+              answer: "this is a1"
+            },
+            {
+              question: "this is q2",
+              answer: "this is a2"
+            },
+            {
+              question: "this is q3",
+              answer: "this is a3"
+            }
+          ]
+        };
+        var expected = {
+          Questions: [
+            {
+              question: "this is q1",
+              answer: cencorString
+            },
+            {
+              question: "this is q2",
+              answer: cencorString
+            },
+            {
+              question: "this is q3",
+              answer: cencorString
+            }
+          ]
+        };
+        var newObj = helper._censorDeep(original, 'Questions.*.answer'.split('.'));
+        expect(newObj).to.have.deep.equal(expected);
+      });
+
+      it('should be able to censor data in array with normal position', function() {
+        var original = {
+          Questions: [
+            {
+              question: "this is q1",
+              answer: "this is a1"
+            },
+            {
+              question: "this is q2",
+              answer: "this is a2"
+            },
+            {
+              question: "this is q3",
+              answer: "this is a3"
+            }
+          ]
+        };
+        var expected = {
+          Questions: [
+            {
+              question: "this is q1",
+              answer: cencorString
+            },
+            {
+              question: "this is q2",
+              answer: "this is a2"
+            },
+            {
+              question: "this is q3",
+              answer: "this is a3"
+            }
+          ]
+        };
+        var newObj = helper._censorDeep(original, 'Questions.0.answer'.split('.'));
+        expect(newObj).to.have.deep.equal(expected);
+      });
+
+      it('should be able to censor data in array with nested array', function() {
+        var original = {
+          Questions: [
+            {
+              question: ["haha", "haha"],
+              answer: "this is a1"
+            },
+            {
+              question: ["haha", "haha"],
+              answer: "this is a2"
+            }
+          ]
+        };
+        var expected = {
+          Questions: [
+            {
+              question: ["haha",cencorString],
+              answer: "this is a1"
+            },
+            {
+              question: ["haha",cencorString],
+              answer: "this is a2"
+            }
+          ]
+        };
+        var newObj = helper._censorDeep(original, 'Questions.*.question.1'.split('.'));
+        expect(newObj).to.have.deep.equal(expected);
+      });
+
+      it('should be able to censor data in array with nested empty array', function() {
+        var original = {
+          Questions: [
+            {
+              question: ["hahe"],
+              answer: "this is a1"
+            },
+            {
+              question: ["haha", "haha"],
+              answer: "this is a2"
+            }
+          ]
+        };
+        var expected = {
+          Questions: [
+            {
+              question: ["hahe"],
+              answer: "this is a1"
+            },
+            {
+              question: ["haha",cencorString],
+              answer: "this is a2"
+            }
+          ]
+        };
+        var newObj = helper._censorDeep(original, 'Questions.*.question.1'.split('.'));
+        expect(newObj).to.have.deep.equal(expected);
+      });
     });
     context('#censor', function() {
       it('should be able to censor deep config', function() {
